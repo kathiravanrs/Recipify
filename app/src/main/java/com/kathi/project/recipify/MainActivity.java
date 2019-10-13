24ff895fd5ff85533d75327096bc36e72d6f2a59
kathi <kathiravanrs@gmail.com>
2019-10-13 11:43:04 +0530

    Separate page for results

--- app/src/main/java/com/kathi/project/recipify/MainActivity.java
+++ app/src/main/java/com/kathi/project/recipify/MainActivity.java
@@ -1,14 +1,20 @@
 package com.kathi.project.recipify;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.wonderkiln.camerakit.CameraKitError;
 import com.wonderkiln.camerakit.CameraKitEvent;
@@ -17,6 +23,8 @@ import com.wonderkiln.camerakit.CameraKitImage;
 import com.wonderkiln.camerakit.CameraKitVideo;
 import com.wonderkiln.camerakit.CameraView;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -29,9 +37,8 @@ public class MainActivity extends AppCompatActivity {
     private static final int INPUT_SIZE = 224;
 
     private Classifier classifier;
-
     private Executor executor = Executors.newSingleThreadExecutor();
-    private Button btnDetectObject, btnToggleCamera;
+    private Button btnDetectObject, btnToggleCamera, btnToggleFlash;
     private CameraView cameraView;
 
     @Override
@@ -40,8 +47,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_test);
         cameraView = findViewById(R.id.cameraView);
 
-        btnToggleCamera = findViewById(R.id.btnToggleCamera);
         btnDetectObject = findViewById(R.id.btnDetectObject);
+        btnToggleCamera = findViewById(R.id.btnToggleCamera);
+        btnToggleFlash = findViewById(R.id.btnToggleFlash);
 
         cameraView.addCameraKitListener(new CameraKitEventListener() {
             @Override
@@ -51,22 +59,20 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onError(CameraKitError cameraKitError) {
-
             }
 
             @Override
             public void onImage(CameraKitImage cameraKitImage) {
 
                 Bitmap bitmap = cameraKitImage.getBitmap();
-
+                saveImage(bitmap);
                 bitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false);
-
-
                 final List<Classifier.Recognition> results = classifier.recognizeImage(bitmap);
+
 
                 Intent myIntent = new Intent(MainActivity.this, ResultActivity.class);
                 myIntent.putExtra("result", results.toString());
-                myIntent.putExtra("image", bitmap);//Optional parameters
+                myIntent.putExtra("image", bitmap);
                 MainActivity.this.startActivity(myIntent);
 
             }
@@ -77,12 +83,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-//        btnToggleCamera.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                cameraView.toggleFacing();
-//            }
-//        });
+
+        btnToggleCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cameraView.toggleFacing();
+            }
+        });
+
+        btnToggleFlash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cameraView.toggleFlash();
+                Toast.makeText(MainActivity.this, ""+cameraView.getFlash(), Toast.LENGTH_LONG).show();
+            }
+        });
 
         btnDetectObject.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -143,5 +158,35 @@ public class MainActivity extends AppCompatActivity {
                 btnDetectObject.setVisibility(View.VISIBLE);
             }
         });
+    }
+
+    private void saveImage(Bitmap bm){
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(MainActivity.this,"Permission Not Granted", Toast.LENGTH_LONG).show();
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    5);
+        }
+        else{
+            Toast.makeText(this,"Permission Granted", Toast.LENGTH_LONG).show();
+        }
+
+        String root = Environment.getExternalStorageDirectory().toString();
+        File myDir = new File("/req_images");
+        myDir.mkdirs();
+        String fname = "Image.jpg";
+        File file = new File(myDir, fname);
+        Log.i("TAG", "" + file);
+        if (file.exists())
+            file.delete();
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            bm.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/kathi/project/recipify/ResultActivity.java
+++ app/src/main/java/com/kathi/project/recipify/ResultActivity.java
@@ -3,6 +3,7 @@ package com.kathi.project.recipify;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -10,23 +11,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 public class ResultActivity extends AppCompatActivity {
-    TextView textViewResult;
-    ImageView imageResult;
+    TextView foodTextView, warningView;
+    Button hyperLinkView2, hyperLinkView3, nutrientView;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.result_view);
+        setContentView(R.layout.result_view_test);
 
-        textViewResult = findViewById(R.id.textViewResult);
-        imageResult = findViewById(R.id.imageViewResult);
+        foodTextView = findViewById(R.id.food_textview);
+        imageView = findViewById(R.id.imageView);
 
         Intent intent = getIntent();
         String TextValue = intent.getStringExtra("result");
         Bitmap bitmap = (Bitmap) intent.getParcelableExtra("image");
 
-        textViewResult.setText(TextValue);
-        imageResult.setImageBitmap(bitmap);
+        foodTextView.setText(TextValue);
+        imageView.setImageBitmap(bitmap);
 
     }
 }
